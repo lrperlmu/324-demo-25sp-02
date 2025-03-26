6909456878c64b214c80c0bb462f2f47e910b30a
Leah Perlmutter <leahp62@gmail.com>
2025-03-26 15:48:14 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,11 +1,11 @@
 print("test 1: 3 == 3")
-if (3 == 3):
+if 3 == 3:
   print("success")
 else:
   print("fail")
 
 print("test 2: 3 == 2")
-if (3 == 2):
+if 3 == 2:
   print("success")
 else:
   print("fail")
